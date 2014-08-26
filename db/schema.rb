f44d7bc28d01281d15022bf63300ebf7972e742d
shareef <shareef.dudekula@people10.com>
2014-08-26 17:50:44 +0530

    added postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140826060632) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: true do |t|
     t.string   "name"
     t.string   "author_name"
